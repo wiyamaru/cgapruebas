64dc97f104c8eb6d34292be6053ded1a89856d10
Wilmar Martinez <wilmar.martinezr@gmail.com>
2023-02-27 18:20:02 -0500

    test

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="row">
 	<div class="text-end mt-2">
-		<p class="fs-6">Copyright 2023 - Construcciones Gómez Asociados S.A.S</p>
+		<p class="fs-6">Copyright 2022 - Construcciones Gómez Asociados S.A.S</p>
 	</div>
 </div>
