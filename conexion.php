bfb40f173af1f7995b9d4963533bc3860a198ba3
yemago <marcela2903@gmail.com>
2023-03-06 22:03:01 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -3,21 +3,30 @@
 	$Host = '127.0.0.1';
 	
 	$Username = 'u587619498_cgapruebas';
-	$Password = 'Marce1022@';	
+	$Password = 'Marce1022@.';	
 	$dbName = 'u587619498_cgapruebas';
 
 	//Crear conexion mysql
-	$conexion = new mysqli($Host, $Username, $Password, $dbName);
-	$acentos = $conexion->query("SET NAMES 'utf8'");
-	
-	//revisar conexion
-	if($conexion->connect_error)
+	try
 	{
+		$conexion = new mysqli($Host, $Username, $Password, $dbName);
+		$acentos = $conexion->query("SET NAMES 'utf8'");
+		return $conexion;
+		
+		//revisar conexion
+		if($conexion->connect_error)
+		{
+			
+		}
+	}
+	catch(exception $e)
+	{
+		//echo "Error: ".$e;
 		//$_SESSION["iniciar_sesion"]="2";
-		$_SESSION["mensaje_error"]="No fue posible realizar la conexión a la base de datos.";
+		$_SESSION["mensaje_error"]="No fue posible realizar la conexión a la base de datos. ".$e;
 		$_SESSION["enviar_formulario"]=true;
 		header("Location: ./");
 		exit();
 	}
-	return $conexion;
+	
 ?>
