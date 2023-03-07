3da6c995655b8ef27c9c4fd3e43d2bd23535cdd5
yemago <marcela2903@gmail.com>
2023-03-06 22:10:54 -0500

    conex

--- conexion.php
+++ conexion.php
@@ -1,9 +1,4 @@
 <?php
-	if(session_id() == '') 
-	{
-		session_start();
-	}
-
 	//$Host = 'mysql.hostinger.co';
 	$Host = '127.0.0.1';
 	
@@ -12,22 +7,17 @@
 	$dbName = 'u587619498_cgapruebas';
 
 	//Crear conexion mysql
-	try
+	$conexion = new mysqli($Host, $Username, $Password, $dbName);
+	$acentos = $conexion->query("SET NAMES 'utf8'");
+	
+	//revisar conexion
+	if($conexion->connect_error)
 	{
-		$conexion = new mysqli($Host, $Username, $Password, $dbName);
-	}
-	catch(exception $e)
-	{
-		//echo "Error: ".$e;
 		//$_SESSION["iniciar_sesion"]="2";
-		$_SESSION["mensaje_error"]="No fue posible realizar la conexión a la base de datos. ".$e;
+		$_SESSION["mensaje_error"]="No fue posible realizar la conexión a la base de datos.";
 		$_SESSION["enviar_formulario"]=true;
 		header("Location: ./");
 		exit();
 	}
-	finally
-	{
-		$acentos = $conexion->query("SET NAMES 'utf8'");
-		return $conexion;
-	}
+	return $conexion;
 ?>
