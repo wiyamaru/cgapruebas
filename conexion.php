74e8258fa0f122eea85eedfa5dfb61a14c746897
yemago <marcela2903@gmail.com>
2023-03-06 19:23:30 -0500

    actualiza conexion

--- conexion.php
+++ conexion.php
@@ -2,20 +2,21 @@
 	$Host = 'mysql.hostinger.co';
 	//$Host = '127.0.0.1';
 	$Username = 'u587619498_cgapruebas';
-	//$Username = 'wiyamaru@gmail.com@';
-	$Password = 'Marce1022@';	
-	//$Password = 'Wiyamaru07';
+	//$Username = 'root@';
+	//$Password = 'Marce1022@';	
+	$Password = 'root';
 	$dbName = 'u587619498_cgapruebas';
 
 	//Crear conexion mysql
 	$conexion = new mysqli($Host, $Username, $Password, $dbName);
 	$acentos = $conexion->query("SET NAMES 'utf8'");
-
+	
 	//revisar conexion
 	if($conexion->connect_error)
 	{
 		//$_SESSION["iniciar_sesion"]="2";
 		$_SESSION["mensaje_error"]="No fue posible realizar la conexión a la base de datos.";
+		$_SESSION["enviar_formulario"]=true;
 		header("Location: ./");
 		exit();
 	}
